a316a08d1622f7aa5ec75a2407cb78305a96053b
Abhiyan Shrestha <iabhiyaan@gmail.com>
2023-04-07 19:17:05 +0545

    fix: checkbox width

--- src/components/Fields.vue
+++ src/components/Fields.vue
@@ -7,17 +7,18 @@
         :type="field.type"
         :placeholder="`${field.placeholder} ${field.required ? '*' : ''}`"
         :required="field.required"
-        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md"
         :class="[
           field.type === 'checkbox'
             ? 'focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded'
-            : '',
+            : ' w-full',
         ]"
       />
+
       <label
         v-if="field.type === 'checkbox'"
         :for="field.id"
-        class="ml-3 text-sm font-medium text-gray-700"
+        class="ml-2 block text-sm text-gray-900"
         >{{ field.label }}</label
       >
     </div>
